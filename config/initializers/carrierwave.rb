0df49ebbd98c9e49ef20a077256f8f60d653cafc
ming <nicholaskhuu@lycos.com>
2012-03-06 19:19:46 -0500

    fixing cw1

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ if Rails.env.production?
       aws_secret_access_key: ENV['IxQZlVesGvUu2Ta6CW2DKYcRhqhCUdEnqyEzcjzk'],
       region: 'us-east-1'
     }
-    config.fog_host = "//#{ENV['bubboli']}.s3.amazonaws.com"
+
     config.fog_directory = ENV['bubboli']
     config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
     config.storage = :fog
@@ -14,7 +14,6 @@ if Rails.env.production?
 elsif Rails.env.development?
   CarrierWave.configure do |config|
     config.storage = :file
-    config.enable_processing = false
   end
 elsif Rails.env.test? or Rails.env.cucumber?
   CarrierWave.configure do |config|
