a2b468aba049702d5cb57cc182038491d24c1e05
ming <nicholaskhuu@lycos.com>
2013-03-14 12:04:42 -0400

    added invitation back into confirm_or_delete method so it finds the invitation id and not the user id

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -7,7 +7,7 @@ class InvitationsController < ApplicationController
   end
   
   def confirm_or_delete
-    @invitation = Invitation.find(params[:id])
+    @invitation = Invitation.find(params[:invitation][:id])
     @child = Child.find_by_id params[:child] if params[:child].present?
     if @child
       @invitation.confirm_as_child(@child)
