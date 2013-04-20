568dbae6a1631c503366de7b3557f5bbba8c4847
ming <nicholaskhuu@lycos.com>
2013-04-20 17:36:27 -0400

    added method to send gift alert

--- app/models/gift_accession.rb
+++ app/models/gift_accession.rb
@@ -7,11 +7,16 @@ class GiftAccession < ActiveRecord::Base
   belongs_to :giver, :class_name => "User", :foreign_key => 'user_id'
   
   before_save :approve_gifts_from_parents
+  after_save :send_gift_alert_email, :on => :create
   
   def approve_gifts_from_parents
     if self.giver.is_parent_of?(self.giftee)
       self.approved = "true"
     end
+  end
+  
+  def send_gift_alert_email
+    GiftAlertMailer.gift_alert(self).deliver
   end
     
 end
