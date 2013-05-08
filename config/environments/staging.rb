41f40b69d4d84f606a998282501ab9517da29502
ming <nicholaskhuu@lycos.com>
2013-05-08 15:10:28 -0400

    added default host url

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -66,6 +66,9 @@ BubboliKids::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # For email confirmation email
+  config.action_mailer.default_url_options = { :host => 'bubboli-kids-staging.herokuapp.com/' }
   
 
   
