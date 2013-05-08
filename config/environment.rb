8880fc166b4e6ec772c00a905d3b0a45d032361b
ming <nicholaskhuu@lycos.com>
2013-05-08 13:38:04 -0400

    changed mandrill config from staging.rb to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BubboliKids::Application.initialize!
+
+# mandrill
+config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => "bubboli",
+    :password  => "42tOVqQSZKWbeUjFrx2fVw",
+    :authentication => 'login' # Mandrill supports 'plain' or 'login'
+  }
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -67,15 +67,5 @@ BubboliKids::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  # mandrill
-  config.action_mailer.smtp_settings = {
-      :address   => "smtp.mandrillapp.com",
-      :port      => 587,
-      :enable_starttls_auto => true, # detects and uses STARTTLS
-      :user_name => "bubboli",
-      :password  => "42tOVqQSZKWbeUjFrx2fVw",
-      :authentication => 'login' # Mandrill supports 'plain' or 'login'
-    }
-  
   #config.middleware.use CanonicalHost, 'www.bubboli.com'
 end
