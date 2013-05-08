10865e927a2cd7e15aee0b0731b52f1bd3b14b61
ming <nicholaskhuu@lycos.com>
2013-05-08 19:08:30 -0400

    fix gift index

--- app/uploaders/gift_image_uploader.rb
+++ app/uploaders/gift_image_uploader.rb
@@ -41,11 +41,11 @@ class GiftImageUploader < CarrierWave::Uploader::Base
   end
   
   version :large_pic, :from_version => :ex_large_pic do
-    process :resize_to_fill => [268, 360]
+    process :resize_to_limit => [268, 268]
   end
  
   version :thumb_pic, :from_version => :ex_large_pic do
-    process :resize_to_fill => [170, 170]
+    process :resize_to_limit => [170, 170]
   end
     
   version :sm_thumb_pic, :from_version => :ex_large_pic do
