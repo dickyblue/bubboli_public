04d746c1ef862fedce8e3fabb25688df32761b01
ming <nicholaskhuu@lycos.com>
2013-05-31 16:06:04 -0400

    commented out error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,25 +1,25 @@
 class ErrorsController < ApplicationController
   
-  def error_404
-    @not_found_path = params[:not_found]
-  end
- 
-  def error_500
-  end
-
-  ERRORS = [ 
-    :internal_server_error,
-    :not_found,
-    :unprocessable_entity
-  ].freeze
-  
-  ERRORS.each do |e|
-    define_method e do
-      respond_to do |format|
-        format.html { render 'error_404', :status => e }
-        format.any { head e }
-      end
-    end
-  end
+  # def error_404
+  #   @not_found_path = params[:not_found]
+  # end
+  #  
+  # def error_500
+  # end
+  # 
+  # ERRORS = [ 
+  #   :internal_server_error,
+  #   :not_found,
+  #   :unprocessable_entity
+  # ].freeze
+  # 
+  # ERRORS.each do |e|
+  #   define_method e do
+  #     respond_to do |format|
+  #       format.html { render 'error_404', :status => e }
+  #       format.any { head e }
+  #     end
+  #   end
+  # end
 
 end
