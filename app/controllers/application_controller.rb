66f6034365d4e7078de5c6634b15b52ab4767bdd
ming <nicholaskhuu@lycos.com>
2013-05-31 16:13:23 -0400

    uncommented error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,15 @@ class ApplicationController < ActionController::Base
   
   include SessionsHelper
   
-  # rescue_from Exception,  with: lambda { |exception| render_error 500, exception }
-  # rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
-  # rescue_from Timeout::Error, with: lambda { |exception| render_error 500, exception }
-  # 
-  # private
-  # def render_error(status=500, exception=nil)
-  #   respond_to do |format|
-  #     format.html { render template: "errors/error_#{status}", layout: 'layouts/application', status: status }
-  #     format.all { render nothing: true, status: status }
-  #   end
-  # end
+  rescue_from Exception,  with: lambda { |exception| render_error 500, exception }
+  rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
+  rescue_from Timeout::Error, with: lambda { |exception| render_error 500, exception }
+  
+  private
+  def render_error(status=500, exception=nil)
+    respond_to do |format|
+      format.html { render template: "errors/error_#{status}", layout: 'layouts/application', status: status }
+      format.all { render nothing: true, status: status }
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,6 @@ module BubboliKids
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    # config.exceptions_app = self.routes
+    config.exceptions_app = self.routes
   end
 end
