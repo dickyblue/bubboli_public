1a8fd6c78744883cb6aca02e20751b1fe804f2ca
ming <nicholaskhuu@lycos.com>
2013-06-04 00:00:58 -0400

    changed the email subject for gift recommendation email

--- app/mailers/gift_recommendation_mailer.rb
+++ app/mailers/gift_recommendation_mailer.rb
@@ -9,14 +9,14 @@ class GiftRecommendationMailer < ActionMailer::Base
       @gift_one_image = @gift_one.gift_images.first.image_url(:thumb_pic)    
       @gift_two_image = @gift_two.gift_images.first.image_url(:thumb_pic)    
       @gift_one_desc = @gift_one.description.truncate(100)
-      mail :to => relationship.user.email, :subject => "Test Mandrill"
+      mail :to => relationship.user.email, :subject => "Your gift recommendations for #{@child.first_name}"
     end
 
     def gift_recommendation(relationship)
       @user = relationship.user.first_name
       @child = relationship.child
       @gifts = Gift.gift_by_pref_all_cat(@child, relationship, limit=6)      
-      mail :to => relationship.user.email, :subject => "Test Mandrill"
+      mail :to => relationship.user.email, :subject => "#{@child.first_name}'s birthday is coming up! Here are the recommendations you requested."
     end
 
     
