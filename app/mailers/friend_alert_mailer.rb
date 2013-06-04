2b2c1ad2cf2237696914323a73055c5cac25acfc
ming <nicholaskhuu@lycos.com>
2013-06-04 13:06:55 -0400

    fix friend alert email with pic

--- app/mailers/friend_alert_mailer.rb
+++ app/mailers/friend_alert_mailer.rb
@@ -5,6 +5,7 @@ class FriendAlertMailer < ActionMailer::Base
     @user = relationship.user
     @child = relationship.child
     @parent = @child.parents.first.user
+    @user_profile = @user.image ? image_tag @user.image_url(:thumb) : "profile_pic_default.jpg"
     mail :to => @parent.email, :subject => "#{@user.first_name} wants to be reminder of #{@child.first_name}'s birthday"
   end  
   
