33caa62078266575c2240e0b89fcd5de53436d6b
ming <nicholaskhuu@lycos.com>
2013-07-01 18:12:05 -0400

    changed subject in invite email

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -5,7 +5,7 @@ class InvitationMailer < ActionMailer::Base
     @invitation = invitation
     @sender = @invitation.sender
     @child = @invitation.child
-    mail :to => invitation.recipient_email, :subject => "Invitation for Bubboli"
+    mail :to => invitation.recipient_email, :subject => "#{@sender.first_name} Invitation for Bubboli"
   end
   
 end
