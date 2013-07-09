63f6fedb8e5cd331d263d17248741c64dc5ac938
ming <nicholaskhuu@lycos.com>
2013-07-09 00:23:49 -0400

    made blog images also public

--- app/uploaders/blog_image_uploader.rb
+++ app/uploaders/blog_image_uploader.rb
@@ -17,6 +17,10 @@ class BlogImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
+  configure do |c|
+    c.fog_public = true # or false
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
