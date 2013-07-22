10764addde601ee3e6fbfdef51311e0d70d72712
ming <nicholaskhuu@lycos.com>
2013-07-22 17:19:24 -0400

    added the send welcome email at confirm

--- app/models/user.rb
+++ app/models/user.rb
@@ -86,6 +86,7 @@ class User < ActiveRecord::Base
   def confirm!
     self.confirmed = true
     self.confirmation_token = nil
+    UserMailer.delay.welcome_email(self)
     save(:validate => false)
   end
   
