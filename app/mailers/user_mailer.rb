cdb3d58d3155abab49bf70fdb75bb3878ae8565d
ming <nicholaskhuu@lycos.com>
2013-07-23 18:57:14 -0400

    changed from email for welcome email to ming at bubboli

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,18 @@
 class UserMailer < ActionMailer::Base
-  default from: "contact@bubboli.com"
   
   def account_confirmation(user)
     @user = user
-    mail :to => user.email, :subject => "Email Activation for Bubboli"
+    mail :from => "contact@bubboli.com", :to => user.email, :subject => "Email Activation for Bubboli"
   end
   
   def password_reset(user)
     @user = user
-    mail :to => user.email, :subject => "Password Reset Instructions"
+    mail :from => "contact@bubboli.com", :to => user.email, :subject => "Password Reset Instructions"
   end
   
   def welcome_email(user)
     @user = user
-    mail :to => user.email, :subject => "Welcome to the Bubboli community"
+    mail :from => "ming@bubboli.com", :to => user.email, :subject => "Welcome to the Bubboli community"
   end
   
 end
