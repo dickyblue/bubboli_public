721556daeebd25eed99db5e1526c017f572aba5e
ming <nicholaskhuu@lycos.com>
2013-08-01 16:57:16 -0400

    replaced input api keys to env

--- config/initializers/amazon-ecs.rb
+++ config/initializers/amazon-ecs.rb
@@ -1,3 +1,3 @@
 require 'amazon/ecs'
 
-Amazon::Ecs.options = { :associate_tag => 'bubboli-20', :AWS_access_key_id => 'AKIAI2WTSOAPR7WAFDCQ', :AWS_secret_key => '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'}
+Amazon::Ecs.options = { :associate_tag => 'bubboli-20', :AWS_access_key_id => ENV['AMAZON_ACCESS_KEY'], :AWS_secret_key => ENV['AMAZON_SECRET_KEY']}
