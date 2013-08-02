0e1f08118829eaf69586eaa331b4932c1fd8bd8e
ming <nicholaskhuu@lycos.com>
2013-08-02 14:48:45 -0400

    added brief review summary

--- config/initializers/amazon-ecs.rb
+++ config/initializers/amazon-ecs.rb
@@ -2,5 +2,3 @@ require 'amazon/ecs'
 
 Amazon::Ecs.options = { :associate_tag => 'bubboli-20', :AWS_access_key_id => ENV['AMAZON_ACCESS_KEY'], :AWS_secret_key => ENV['AMAZON_SECRET_KEY']}
 
-
-# Amazon::Ecs.options = { :associate_tag => 'bubboli-20', :AWS_access_key_id => 'AKIAJ4ZKFIFTSOJZ62FA', :AWS_secret_key => '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'}
