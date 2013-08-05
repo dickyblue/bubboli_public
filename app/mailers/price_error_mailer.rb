dee20c4f49adac13a0fe72850a99ea914c1436a4
ming <nicholaskhuu@lycos.com>
2013-08-05 16:59:51 -0400

    changed price error email

--- app/mailers/price_error_mailer.rb
+++ app/mailers/price_error_mailer.rb
@@ -1,8 +1,7 @@
 class PriceErrorMailer < ActionMailer::Base
 
   def price_error(user)
-    user = User.where(:email => "ming@bubboli.com")
-    gifts = Gift.where(:price_error => true)
+    @gifts = Gift.where(:price_error => true)
     mail :from => "contact@bubboli.com", :to => user.email, :subject => "Price Errors"
   end
 
