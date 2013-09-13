7f4ee36237073ea75d8bd11c68c60b7b366b44f4
ming <nicholaskhuu@lycos.com>
2013-09-13 15:42:59 -0400

    changed amazon associate tag

--- config/initializers/amazon-ecs.rb
+++ config/initializers/amazon-ecs.rb
@@ -1,4 +1,4 @@
 require 'amazon/ecs'
 
-Amazon::Ecs.options = { :associate_tag => 'bubboli-20', :AWS_access_key_id => ENV['AMAZON_ACCESS_KEY'], :AWS_secret_key => ENV['AMAZON_SECRET_KEY']}
+Amazon::Ecs.options = { :associate_tag => 'bubb-amaz-20', :AWS_access_key_id => ENV['AMAZON_ACCESS_KEY'], :AWS_secret_key => ENV['AMAZON_SECRET_KEY']}
 
