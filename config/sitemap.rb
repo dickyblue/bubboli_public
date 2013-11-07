1d33094aeb9e5829ba6329e724f406c0cee424b8
ming <nicholaskhuu@lycos.com>
2013-11-07 16:40:36 -0500

    changed adapter back to carrierwave

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,9 +1,9 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://www.bubboli.com"
-SitemapGenerator::Sitemap.sitemaps_host = ENV['SITEMAP_HOST']
+SitemapGenerator::Sitemap.sitemaps_host = "http://s3.amazonaws.com/sitemap-generator/"
 SitemapGenerator::Sitemap.public_path = 'tmp/'
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
 
 
 SitemapGenerator::Sitemap.create do
