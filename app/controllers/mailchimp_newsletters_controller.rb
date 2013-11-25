a52ee224d22a4fb5d43c7ad61a457d59bc3acd3a
ming <nicholaskhuu@lycos.com>
2013-11-25 17:58:16 -0500

    added gibbon

--- app/controllers/mailchimp_newsletters_controller.rb
+++ app/controllers/mailchimp_newsletters_controller.rb
@@ -3,7 +3,7 @@ class MailchimpNewslettersController < ApplicationController
   def subscribe
     gb = Gibbon::API.new(ENV['MAILCHIMP_API_KEY'])
     list_id = ENV['MAILCHIMP_NEWSLETTER_LIST_ID']
-    gb.lists.subscribe({:id => "#{list.id}", :email => {:email => params[:email]}})
+    gb.lists.subscribe({:id => "#{list_id}", :email => {:email => params[:email]}})
   end
   
 end
