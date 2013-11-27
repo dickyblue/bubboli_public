ebf0a13fd98e77c7c1ea3e5d5c8a227998d0a1c2
ming <nicholaskhuu@lycos.com>
2013-11-27 00:21:13 -0500

    added popular products to thank you for signing up to newsletter page

--- app/controllers/mailchimp_newsletters_controller.rb
+++ app/controllers/mailchimp_newsletters_controller.rb
@@ -4,6 +4,7 @@ class MailchimpNewslettersController < ApplicationController
     gb = Gibbon::API.new(ENV['MAILCHIMP_API_KEY'])
     list_id = ENV['MAILCHIMP_NEWSLETTER_LIST_ID']
     gb.lists.subscribe({:id => "#{list_id}", :email => {:email => params[:email]}, :double_optin => false })
+    @gifts = Gift.random(5)
   end
   
 end
